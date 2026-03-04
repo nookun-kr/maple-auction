849d8d60debdbc078fe3fbea60cebfdb001f4732
nookun-kr <wing4465@gmail.com>
2026-03-04 22:07:08 +0900

    feat: 프로젝트 초기 세팅 및 도커 연동 완료

---
+++ maple-auction/src/main/java/com/maple/auction/MapleAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.maple.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleAuctionApplication.class, args);
+    }
+
+}
---
+++ maple-auction/src/test/java/com/maple/auction/MapleAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
