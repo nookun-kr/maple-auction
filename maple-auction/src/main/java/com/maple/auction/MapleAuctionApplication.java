608408f98a70f04e766e9b466953452ead3fcbba
nookun-kr <wing4465@gmail.com>
2026-03-04 22:22:47 +0900

    테스트 커밋

--- maple-auction/src/main/java/com/maple/auction/MapleAuctionApplication.java
+++ maple-auction/src/main/java/com/maple/auction/MapleAuctionApplication.java
@@ -8,6 +8,9 @@ public class MapleAuctionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MapleAuctionApplication.class, args);
+        //이준원은 봐라
+
+
     }
 
 }
